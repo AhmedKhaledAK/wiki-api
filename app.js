edb5752165601ccf1e58826d4ff4184e87d201ec
Ahmed Khaled <ahmedkhaledabab@gmail.com>
2019-09-09 14:38:25 +0200

    connected to database, added a schema and a collection

--- app.js
+++ app.js
@@ -6,6 +6,16 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
+mongoose.connect("mongodb://localhost:27017/wikidb", {useNewUrlParser: true});
+
+const articleSchema = new mongoose.Schema({
+  title: String,
+  titleLower: String,
+  content: String
+});
+
+const Article = mongoose.model("Article", articleSchema);
+
 app.listen(3000, function(){
   console.log("Server is running on port 3000");
 });
