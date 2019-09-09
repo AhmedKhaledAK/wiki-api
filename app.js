34014395510f8122499b260c9c3948f9a8179373
Ahmed Khaled <ahmedkhaledabab@gmail.com>
2019-09-09 14:56:13 +0200

    added the middleware function of body-parser

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ const app = express();
 
 app.set('view engine','ejs');
 
+app.use(bodyParser.urlencoded({extended: true}));
+
 app.use(express.static("public"));
 
 mongoose.connect("mongodb://localhost:27017/wikidb", {useNewUrlParser: true});
